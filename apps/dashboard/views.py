64ee2c0e5772b3b73d555d0f420e8a6066b15575
omkargwagholikar <omkargwagholikar@gmail.com>
2025-03-19 01:27:18 +0530

    WIP: README and Docs

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import status
-
+from ..videos.models import *
+from ..videos.serializers import *
 # Create your views here.
 def index(request):
-    return render(request, "index.html")    
+    key_word_obj = KeyWordEntry.objects.all()
+    key_words = KeyWordEntrySerializer(key_word_obj, many=True)
+    print(key_words.data)
+    return render(request, "index.html", {"key_words": key_words.data})    
 
 def health_check(request):
     data = {
